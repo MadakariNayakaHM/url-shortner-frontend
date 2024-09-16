747aa57aa70e64c4fe5efbf83b93884d700d1f35
MadakariNayakaHM <madakarinayakadinesh6282@gmail.com>
2024-09-16 11:18:24 +0530

    port change

--- url-shortner/vite.config.ts
+++ url-shortner/vite.config.ts
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
+    host:'0.0.0.0',
     open:true,
-    port:3000,
+    port:parseInt(process.env.PORT || '3000') ,
   }
 
 })
